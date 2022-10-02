a17551ba04a4de93817abdba5027e5250b84f57a
sanketnighot <sanketnighot25@gmail.com>
2022-10-03 02:28:14 +0530

    UPDATE

--- Controller/mapController.js
+++ Controller/mapController.js
@@ -10,6 +10,7 @@ module.exports.getMerkleProof = async (req, res) => {
     let leaves = Address.map(addr => keccak256(addr))
     let merkleTree = new MerkleTree(leaves, keccak256, {sortPairs: true})
     const rootHash = merkleTree.getHexRoot()
+   // console.log(rootHash)
     let prove = checkAddress // The input
     let hashedAddress = keccak256(prove)
     let proof = merkleTree.getHexProof(hashedAddress)
